f592cee77819251aaecaac78f8fe0f000fae6f2b
justBoringUsername <63564366+justBoringUsername@users.noreply.github.com>
2020-12-06 13:57:17 +0300

    Add files via upload

---
+++ DependencyInjectionContainer/Dependency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DependencyInjectionContainer
+{
+    public class Dependency
+    {
+        public Type Type { get; }
+
+        public LifeType LifeType { get; }
+
+        public object Key { get; }
+
+        public object Instance { get; set; }
+
+        public Dependency(Type type, LifeType lifeType, object key)
+        {
+            Key = key;
+            Type = type;
+            LifeType = lifeType;
+        }
+    }
+}
